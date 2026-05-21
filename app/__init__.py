f3f479f5df085e767de732d8060c33a58f56cc01
mingmaaa <kritonsherpa@gmail.com>
2026-05-21 21:39:31 +0545

    /login routes to login.html

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,12 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    # register blueprints
+    from .routes import auth as auth_bp
+    app.register_blueprint(auth_bp.bp)
+
     @app.errorhandler(404)
     def page_not_found(e):
         return "PAGE NOT FOUND",404
-    
+
     return app
