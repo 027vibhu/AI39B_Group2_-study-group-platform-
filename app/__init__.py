57fed6740f427702bbb2d3f66fd5f07c7b7584b4
mingmaaa <kritonsherpa@gmail.com>
2026-05-21 21:53:07 +0545

    updated __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,7 @@ def create_app():
     app = Flask(__name__)
 
     # register blueprints
-    from .routes import auth as auth_bp
+    from app.routes import auth as auth_bp
     app.register_blueprint(auth_bp.bp)
 
     @app.errorhandler(404)
