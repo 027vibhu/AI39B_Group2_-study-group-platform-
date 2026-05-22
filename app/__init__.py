67caabfcb83d5e1f5d65478f581ab4b1e3a64e49
mingmaaa <kritonsherpa@gmail.com>
2026-05-22 09:26:24 +0545

    Added home blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,9 @@ def create_app():
     app = Flask(__name__)
 
     # register blueprints
+    from app.routes import home as home_bp
     from app.routes import auth as auth_bp
+    app.register_blueprint(home_bp.bp)
     app.register_blueprint(auth_bp.bp)
 
     @app.errorhandler(404)
