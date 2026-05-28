e22925c38dbd1901d82a111ab7b2ad5877109aae
mingmaaa <kritonsherpa@gmail.com>
2026-05-28 19:55:50 +0545

    Added BaseController for controllers

---
+++ app/controllers/base_controller.py
@@ -0,0 +1,17 @@
+from flask import render_template, redirect, url_for, session, request, flash
+
+
+class BaseController:
+    """Minimal BaseController to be inherited by concrete controllers.
+
+    Provides small helper wrappers around common Flask response helpers.
+    """
+
+    def render(self, template_name, **context):
+        return render_template(template_name, **context)
+
+    def redirect(self, endpoint, **kwargs):
+        return redirect(url_for(endpoint, **kwargs))
+
+    def get_session_user_id(self):
+        return session.get('user_id')
