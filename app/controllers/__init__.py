5e29b640e0f1d85f657d632bf7b68ff7f403da06
Almira098 <almiradwa@gmail.com>
2026-06-07 15:05:40 +0545

    Added database setup controller

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,3 +1,4 @@
+from app.controllers.database_setup_controller import DatabaseSetupController
 from app.controllers.message_vote_controller import MessageVoteController
 
 # Controller package for the Flask application
